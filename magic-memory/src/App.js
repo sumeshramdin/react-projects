a86e765967b8a98a5653652c1d055e8ee8be0a3b
Sumesh Ramdin <sumeshramdin@gmail.com>
2022-06-20 19:26:19 +0000

    first commit

---
+++ magic-memory/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+	
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App
