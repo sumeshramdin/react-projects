5525efebdca402ae5ceac4fce2e610f17a73ddb1
Sumesh Ramdin <sumeshramdin@gmail.com>
2022-07-08 09:21:50 +0000

    Add form

--- bills/src/components/stage_1.js
+++ bills/src/components/stage_1.js
@@ -1,9 +1,35 @@
+import React,{ useState, useContext, useRef } from 'react'
+import { Button, Form, Alert } from 'react-bootstrap'
+
 
 const Stage1 = () => {
+    const textInput = useRef();
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const value = textInput.current.value;
+
+        console.log(value)
+    }
+
 
     return(
         <>
-            stage 1
+            <Form onSubmit={handleSubmit} className="mt-4">
+                <Form.Group>
+                    <Form.Control
+                        type='text'
+                        placeholder='Add player name'
+                        name='player'
+                        ref={textInput}
+                    />
+                </Form.Group>
+
+                <Button className='miami' variant='primary' type='submit'>
+                    Add player
+                </Button>
+            </Form>
         </>
     )
 }
--- bills/src/context/index.js
+++ bills/src/context/index.js
@@ -6,7 +6,7 @@ const MyContext = React.createContext();
 class MyProvider extends Component {
 
     state = {
-        stage:2
+        stage:1
     }
 
 
